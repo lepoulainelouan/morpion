decb1a4a0dba110fc583385b5bf811bebdef8644
Elouan <elouan.lepoulain@edu.esiee.fr>
2025-12-21 20:28:54 +0100

    VS IAAAA

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
-#include <random>
+#include <cstdlib>
+#include <ctime>
 #include "player.hpp"
 #include "game.hpp"
 
 int main(){
     char board[9]={'1','2','3','4','5','6','7','8','9'};
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    std::cout<<"Choisi ton mode (1=1V1, 2=1vsIA)";
+    int mode;
+    std::cin>>mode;
 player p1=create_player();
-player p2=create_player();
+player p2;
+
+bool vs_ai = false;
+if (mode==2){
+    vs_ai=true;
+    p2.name="R2D2";
+    p2.symbol=(p1.symbol=='X')?'O':'X';
+}else{
+    p2=create_player();
+}
 
 player current =p1;
 int position;
@@ -14,9 +29,16 @@ bool victory=false;
 
 for (int turn=0; turn<9 &&!victory; turn++){
     draw_game_board(board);
+
+    if (vs_ai && current.name=="R2D2"){
+        do{
+            position=std::rand()%9+1;
+        }while (board[position-1]==p1.symbol || board[position-1]==p2.symbol);
+        std::cout<<"R2D2 joue \n";
+    }else{
     std::cout<<current.name<<" A TOI DE JOUER ! ";
     std::cin >>position;
-
+    }
     if ( position<1|| position>9|| board[position -1  ]==p1.symbol|| board[position-1]==p2.symbol){
     std::cout<<"Impossible de placer ici gros nullos \n";
     turn--;
--- src/player.cpp
+++ src/player.cpp
@@ -5,7 +5,7 @@ player create_player(){
     player p;
     std::cout << "Rentre ton pseudo: ";
     std::cin >>p.name;
-    std::cout <<"Choisis ton symbol (ex:x)";
+    std::cout <<"Choisis ton symbol (X ou O)";
     std::cin >>p.symbol;
     return p;
 }
